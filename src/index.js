3c4c2c15cb16cd45b510502cea231084b882527a
quiqueruizm <quiqueruizm@gmail.com>
2018-09-07 13:22:40 +0200

    añadida funcionalidad ordenar por nombre

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const uniqueRandomArray = require('unique-random-array')
 const cervezas = require('./cervezas.json')
+const _ = require('lodash')
 module.exports = {
-  todas: cervezas,
+  todas: _.sortBy(cervezas, ['nombre']),
   miPreferida: 'Ambar',
   alazar: uniqueRandomArray(cervezas)
 }
